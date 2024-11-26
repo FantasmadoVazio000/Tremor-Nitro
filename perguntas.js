cc6f725380634a334130958ef5bb895be9febd73
FantasmadoVazio000 <161712988+FantasmadoVazio000@users.noreply.github.com>
2024-11-26 20:48:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Mistério',
+    'Como nasceu a vida na terra',
+    'Bem ocorreu primeiramente na água, em um primeiro momento surgiram seres primitivos, tais como as bactérias, algas e microrganismos isso há cerca de 3,5 bilhões de anos.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Uruguai?',
+    'A capital do Uruguai é Montevidéu'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Qual é a bomba mais forte tirando a nuclear?',
+    'É a bomba de hidrogénio ou hidrogênio'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que significa Shadows from the grave',
+    'significa sombras do túmulo'
+)
